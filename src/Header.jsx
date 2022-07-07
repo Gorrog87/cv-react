b72f82a4e6c5bdafa227009bf6efa483bdbeadcd
Gorrog87 <gourguesmaxime@gmail.com>
2022-07-07 15:27:24 +0200

    finished Header component

--- src/Header.jsx
+++ src/Header.jsx
@@ -13,40 +13,40 @@ import {
 export default class Header extends React.Component {
   render() {
     return (
-      <div className="header">
-        <img
-          id="picture"
-          src="https://avatars.githubusercontent.com/u/88392395?s=400&u=7f6249d4f819bd3fd6b245a982ec8473858a3ad3&v=4"
-          alt="Maxime Gourgues"
-        />
-        <div>
-          <h1>Maxime Gourgues</h1>
-          <h4>Développeur frontend junior</h4>
+        <div className="header">
+          <img
+            id="picture"
+            src="https://avatars.githubusercontent.com/u/88392395?s=400&u=7f6249d4f819bd3fd6b245a982ec8473858a3ad3&v=4"
+            alt="Maxime Gourgues"
+          />
+          <div className="headerName">
+            <h1>Maxime Gourgues</h1>
+            <h4>Développeur frontend junior</h4>
+          </div>
+          <div className="socialLinks">
+            <a
+              href="https://github.com/Gorrog87"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <FontAwesomeIcon icon="fab fa-github" size="lg" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/maxime-gourgues/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <FontAwesomeIcon icon="fa-brands fa-linkedin" size="lg" />
+            </a>
+            <a
+              href="https://troopl.com/gourguesmaxime"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <FontAwesomeIcon icon="fa-solid fa-folder-open" size="lg" />
+            </a>
+          </div>
         </div>
-        <div className="socialLinks">
-          <a
-            href="https://github.com/Gorrog87"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <FontAwesomeIcon icon="fab fa-github" size="lg" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/maxime-gourgues/"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <FontAwesomeIcon icon="fa-brands fa-linkedin" size="lg" />
-          </a>
-          <a
-            href="https://troopl.com/gourguesmaxime"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <FontAwesomeIcon icon="fa-solid fa-folder-open" size="lg" />
-          </a>
-        </div>
-      </div>
     );
   }
 }
