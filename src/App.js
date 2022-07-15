8951a901c494b8f9f06b55f0978b233dbf80f0b8
Maxime Gourgues <gourguesmaxime@gmail.com>
2022-07-15 12:21:11 +0200

    addded Formation component with stylesheet

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Header from "./Header";
 import Bio from './Bio';
 import Skills from "./Skills";
 import Languages from "./Languages";
+import Formation from "./Formation";
 
 function App() {
   return (
     <div>
       <Header />
       <Bio />
+      <Formation/>
       <Skills />
       <Languages />
     </div>
---
+++ src/Formation.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./formation.css";
+
+export default class Formation extends React.Component {
+  render() {
+    return (
+      <div className="formation">
+        <h3 id="forma">Formations</h3>
+        <h5>
+          Le Wagon - Paris (janvier - mars 2022) : Concepteur / Développeur
+          d'appplications web
+        </h5>
+      </div>
+    );
+  }
+}
