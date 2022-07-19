8a10efa8986ebea9d781b8b4eaa64969282a1364
Maxime Gourgues <gourguesmaxime@gmail.com>
2022-07-19 15:54:22 +0200

    added o'clock school to Formation component

--- src/Formation.jsx
+++ src/Formation.jsx
@@ -7,8 +7,12 @@ export default class Formation extends React.Component {
       <div className="formation">
         <h3 id="forma">Formations</h3>
         <h5>
+          O'clock (octobre 2022 - février 2024) en alternance : Concepteur /
+          Développeur d'appplications web
+        </h5>
+        <h5>
           Le Wagon - Paris (janvier - mars 2022) : Concepteur / Développeur
-          d'appplications web
+          d'appplications web 
         </h5>
       </div>
     );
