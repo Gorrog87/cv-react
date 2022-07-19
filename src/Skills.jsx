08070e1f8d92174d8a10a73d9d74c69f65e0546f
Maxime Gourgues <gourguesmaxime@gmail.com>
2022-07-19 17:43:37 +0200

    added styling transition to icons

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-import  "./skills.css";
+import "./skills.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-
 export default class Skills extends React.Component {
+  /*  const moveIcon = (e) => {
+
+  }; */
+
   render() {
     return (
       <div className="skills">
@@ -13,43 +16,83 @@ export default class Skills extends React.Component {
           </div>
           <ul>
             <div className="duo">
-              <FontAwesomeIcon icon="fa-brands fa-html5" id="html" />
+              <FontAwesomeIcon
+                icon="fa-brands fa-html5"
+                id="html"
+                className="iconHover"
+              />
               <li>HTML</li>
             </div>
             <div className="duo">
-              <FontAwesomeIcon icon="fa-brands fa-css3-alt" id="css" />
+              <FontAwesomeIcon
+                icon="fa-brands fa-css3-alt"
+                id="css"
+                className="iconHover"
+              />
               <li>CSS</li>
             </div>
             <div className="duo">
-              <FontAwesomeIcon icon="fa-brands fa-js-square" id="js" />
+              <FontAwesomeIcon
+                icon="fa-brands fa-js-square"
+                id="js"
+                className="iconHover"
+              />
               <li>JavaScript</li>
             </div>
             <div className="duo">
-              <FontAwesomeIcon icon="fa-brands fa-vuejs" id="vue" />
+              <FontAwesomeIcon
+                icon="fa-brands fa-vuejs"
+                id="vue"
+                className="iconHover"
+              />
               <li>Vue.Js</li>
             </div>
             <div className="duo">
-              <FontAwesomeIcon icon="fa-brands fa-php" id="php"/>
+              <FontAwesomeIcon
+                icon="fa-brands fa-php"
+                id="php"
+                className="iconHover"
+              />
               <li>PHP</li>
             </div>
             <div className="duo">
-              <FontAwesomeIcon icon="fa-solid fa-gem" id="ruby" />
+              <FontAwesomeIcon
+                icon="fa-solid fa-gem"
+                id="ruby"
+                className="iconHover"
+              />
               <li>Ruby / Rails</li>
             </div>
             <div className="duo">
-              <FontAwesomeIcon icon="fa-brands fa-react" id="react" />
+              <FontAwesomeIcon
+                icon="fa-brands fa-react"
+                id="react"
+                className="iconHover"
+              />
               <li>React</li>
             </div>
             <div className="duo">
-              <FontAwesomeIcon icon="fa-brands fa-git-square" id="git" />
+              <FontAwesomeIcon
+                icon="fa-brands fa-git-square"
+                id="git"
+                className="iconHover"
+              />
               <li>Git / Github</li>
             </div>
             <div className="duo">
-              <FontAwesomeIcon icon="fa-solid fa-database" id="sql" />
+              <FontAwesomeIcon
+                icon="fa-solid fa-database"
+                id="sql"
+                className="iconHover"
+              />
               <li>SQL</li>
             </div>
             <div className="duo">
-              <FontAwesomeIcon icon="fa-solid fa-server" id="heroku" />
+              <FontAwesomeIcon
+                icon="fa-solid fa-server"
+                id="heroku"
+                className="iconHover"
+              />
               <li>Heroku</li>
             </div>
           </ul>
@@ -60,23 +103,29 @@ export default class Skills extends React.Component {
           </div>
           <ul>
             <div className="duo">
-              <FontAwesomeIcon icon="fas fa-bullseye" />
+              <FontAwesomeIcon icon="fas fa-bullseye" className="iconHover" />
               <li>Cibler les demandes client</li>
             </div>
             <div className="duo">
-              <FontAwesomeIcon icon="fa-solid fa-palette" />
+              <FontAwesomeIcon
+                icon="fa-solid fa-palette"
+                className="iconHover"
+              />
               <li>UI / UX</li>
             </div>
             <div className="duo">
-              <FontAwesomeIcon icon="fas fa-handshake" />
+              <FontAwesomeIcon icon="fas fa-handshake" className="iconHover" />
               <li>Resolution des conflits</li>
             </div>
             <div className="duo">
-              <FontAwesomeIcon icon="fas fa-comments" />
+              <FontAwesomeIcon icon="fas fa-comments" className="iconHover" />
               <li>Bonne communication</li>
             </div>
             <div className="duo">
-              <FontAwesomeIcon icon="fas fa-assistive-listening-systems" />
+              <FontAwesomeIcon
+                icon="fas fa-assistive-listening-systems"
+                className="iconHover"
+              />
               <li>Empathique</li>
             </div>
           </ul>
