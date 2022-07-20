4fe42ea542c8f8b7b387e368159a3208296f6a56
Maxime Gourgues <gourguesmaxime@gmail.com>
2022-07-20 18:22:22 +0200

    link is free from error message but not redirecting ton the good page for the moment working on it

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
-import Home from "Home";
+import Home from "./Home";
 import Cv from "./Cv";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <Home path="/cv"/>
-{/*       <Route path="/cv"> */}
-        <Cv />
-{/*       </Route> */}
+      <Home />
+      <Route><Cv /></Route>
     </Router>
   );
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import "home.css";
-
+import "./home.css";
+import { Link } from "react-router-dom";
 
 export default class Home extends React.Component {
   render() {
     return(
       <div className="home">
         <h1>Welcome to my Website !</h1>
-        <a href="/cv">About</a>
+        <Link to={"/Cv"}>About</Link>
       </div>
     )
   }
