a39c51dbc38e92d580938c58b1531885f9e931b8
Maxime Gourgues <gourguesmaxime@gmail.com>
2022-07-20 11:21:00 +0200

    added responsiveness for small mobile devices

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -6,7 +6,7 @@ export default class Experiences extends React.Component{
     return (
       <div className="experience">
         <h3 id="title-exp">Dernière expérience professionnelle</h3>
-        <h5>Responsable adjoint - Décathlon Wagram (Paris)</h5>
+        <h5 id="responsable">Responsable adjoint - Décathlon Wagram (Paris)</h5>
         <p id="date-exp"><em>(octobre 2016 - mars 2022)</em></p>
         <ul className="exp-list">
           <li>Formateur France pour les produits de golf</li>
