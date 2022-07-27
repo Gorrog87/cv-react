aca100b648290a7e4e46c1942839f171299f0a8a
Maxime Gourgues <gourguesmaxime@gmail.com>
2022-07-27 15:00:10 +0200

    modified some english typo errors

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className="home">
-      <h1 className="welcome">Welcome to my Website !</h1>
+      <h1 className="welcome">Welcome to my Website!</h1>
       <div className="home-nav-links">
         <Link to="/resume" id="resume">
           Resume
